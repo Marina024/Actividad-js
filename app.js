76451e1ac42f81a3d61c980f5ee6593ab33a1c44
Marina024 <130870027+Marina024@users.noreply.github.com>
2023-06-06 09:46:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+//Trabajo práctico de ejercicios en js de Marina E. Suárez - Curso Desarrollo Web Full Stack UTN - CUR Mercedes
+// Ejercicio 1
+// var nombre = "Matias"
+// var apellido = "Rodriguez"
+// var trabajo = "desarrollador"
+// var edad = 25;
+
+// console.log ("Hola soy " + nombre + ", de apellido " + apellido + ". Tengo " + edad + " años " + " y trabajo como " + trabajo);
+
+//Ejercicio 2
+// var edad = 25;
+
+// if(edad == 25){
+//     console.log("El usuario tiene 25 años")
+// } else {
+//     console.log("el usuario no coincide con la edad ingresada")
+// };
+
+//Ejercicio 3 (preguntar)
+// var container = document.getElementById("container")
+// container.innerHTML="<p>Informacion del usuario<li>Nombre: Matias</li><li>Apellido: Rodriguez</li><li>Trabajo: Desarrollador</li><li>Edad: 25</li></p>"
+
+//Ejercicio 4
+// var precio = 100;
+// var iva = 16;
+// var sumatotal = precio + iva;
+// console.log(sumatotal)
+
+//Ejercicio 5
+// var numero = 5;
+// if (numero % 2 == 0){
+//     console.log("El numero es par")}
+//     else{
+//         console.log("El numero es impar")
+//     };
+
+//Ejercicio 7
+// var i = "";
+// for (var i = 1; i <= 20; i++) {
+//     if (i % 3 == 0 && i % 5 == 0)
+//         console.log("FizzBuzz");
+//     else if (i % 3 == 0)
+//         console.log("Fizz");
+//     else if (i % 5 == 0)
+//         console.log("Buzz");
+//     else console.log(i);
+// };
+
+//Ejercicio 8
+// function sumaTotal() {
+//     var suma = 0;
+//     for (var i = 1; i <= 100; i++){
+//         suma += i;
+//     }
+//         return suma;
+// }
+//     console.log(sumaTotal());
+
+//Ejercicio 9
+// var array = [2, 3, 5, 7, 9, 10]
+// for (var i=0; i<array.length; i++){
+//     console.log(i)
+// };
+
+//Ejercicio 10
+// function sumaCaracteres(){
+// var texto = "Estos son ejercicios de js";
+// var textoArray = texto.split("");
+// var suma = 0;
+// for (var i = 0; i <textoArray.length; i++){
+//     suma = i;
+// }
+// return suma;
+// }
+// console.log(sumaCaracteres());
